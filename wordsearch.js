7b48b593e1286075f6e0677be358b8d1f76dd5e4
Ryan Fortin <ryanwfortin@gmail.com>
2022-08-04 01:02:08 +0000

    Passes all core tests does not address diagonal words

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,98 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const reverse = function(data) {
+  return data.split('').reverse().join('');
+};
+const checkIncludesWord = function(text, search) {
+  return !text.includes(search) ? reverse(text).includes(search) : true;
+};
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+  const results = [];
+  const rows = matrix[0].length;
+  const cols = matrix.length;
+
+  for (let r = 0; r < rows; r++) {
+    const result = [];
+    for (let c = 0; c < cols; c++) {
+      result.push(matrix[c][r]);
+    }
+    results.push(result);
+  }
+  return results;
+};
+
+const wordSearch = (letters, word) => {
+
+  // Check the horizontal strings for the search word
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (checkIncludesWord(l,word)) {
+      return true;
+    }
+  }
+  // Check the vertical strings for the search word
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (const l of verticalJoin) {
+    if (checkIncludesWord(l,word)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+
+// // do stuff, row ++ or col ++
+// }
+//            row col
+//            [0] [0]                                [0][7]
+// wordSearch([['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//             ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//             ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//             ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//             ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//             ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//             ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//             ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//             ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L']],
+//            [8][0]                                [8][7]
+//       'SEINFELD');
+
+module.exports = wordSearch;
+
+// describe("#wordSearch()", function() {
+//   it("should return false if the word is not present", function() {
+//     const result = wordSearch([
+//       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//       `awcfqual`
+//       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//       `seinfeld`
+//       ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//       `yfcfqual
+//       ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//       `hmjtevrg`
+//       ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//       ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+//     ], 'FRANK')
+
+//     assert.isFalse(result);
+//   });
+
+//   it("should return true if the word is present", function() {
+//     const result = wordSearch([
+//       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//       ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//       ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//       ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//       ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+//     ], 'SEINFELD')
+
+//     assert.isTrue(result);
+//   });
+// });
